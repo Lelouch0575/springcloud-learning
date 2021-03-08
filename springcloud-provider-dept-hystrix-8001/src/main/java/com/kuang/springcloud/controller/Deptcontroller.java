ad6434dbfb4fe7e3cff80fc1e5c64416d800501a
kirito <1554148583@qq.com>
2021-03-08 16:34:28 +0800

    first commit

---
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/kuang/springcloud/controller/Deptcontroller.java
@@ -0,0 +1,41 @@
+package com.kuang.springcloud.controller;
+
+import com.kuang.springcloud.pojo.Dept;
+import com.kuang.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//提供restful服务
+@RestController
+public class Deptcontroller {
+
+    @Autowired
+    private DeptService deptService;
+
+    @GetMapping("/dept/get/{id}")
+    @HystrixCommand(fallbackMethod = "hystrixGet")
+    public Dept get(@PathVariable("id") Long id) {
+        Dept dept = deptService.queryById(id);
+        if (dept == null) {
+            throw new RuntimeException("id不存在");
+        }
+        return dept;
+    }
+
+    //备选方法
+    public Dept hystrixGet(@PathVariable("id") Long id) {
+        return new Dept()
+                .setDeptno(id)
+                .setDname(id + "没有对应信息，null --@Hystrix")
+                .setDb_source("no this database");
+
+    }
+}
